4e2959a3e44def8e69f823097dc49a2bc4a5bbb3
vurles <vurles27@gmail.com>
2019-04-25 16:12:59 -0400

    cree una promesa que puede leer archivos

--- md-links.js
+++ md-links.js
@@ -1,11 +1,48 @@
-const suma = (a,b) => {
-    return a + b;
-}
-const multi = (y,x) => {
-    return y * x;
-}
+const fs = require('fs');
+const fetch = require ('node-fetch');
+const  markdownLinkExtractor = require('markdown-link-extractor');
+const command = process.argv[2]
 
-module.exports = {
-    suma,
-    multi
+//const markdown = fs.readFileSync(command).toString();ya no va porque es sincona y eso esta en el pasado
+
+//declaro mi funcion que va a leer archivos
+const readingFiles = (files, type) => {
+    //retorno una promesa con los parementros resolve(ejectuta si todo es ta 'bien') y reject(se ejecuta en caso de error)
+    return new Promise((resolve, reject) => {
+        //llamo a mi dependencia fs y le paso el metodo de node readFile con mis parametro de de archivo,tipo Y el callback 
+        fs.readFile(files,type,(error,content) =>{
+            if (error){
+                reject(error)
+            }else{
+                resolve(content)
+            }
+        });
+    });
 }
+//llamo a mi funcion
+readingFiles('./README.md','utf-8')// aqui paso el archivo para leer de forma trucha 
+.then(res =>{
+    console.log(res)
+})
+.catch(err => {
+    console.log(err)
+});
+
+
+
+
+
+// const links = markdownLinkExtractor(readingFiles);
+
+// links.forEach(function (link) {
+//     fetch(link).then((res)=>{
+//         console.log(res.url + ' ' + res.status + ' ' + res.statusText);
+
+//     })
+//     .catch(error => {
+//         console.log(error.message)
+//     })
+// });
+
+// module.exports.links = links;
+//
